aa489b18d688f094821c659f62b6fae44f7f55a9
le2562813 <vespada3@gmail.com>
2015-01-14 13:25:51 -0800

    Hello World

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: Vincent
+ *
+ * Created on January 11, 2015, 2:58 PM
+ */
+
+#include <iostream>
+/*
+ * 
+ */
+int main() {
+    std::cout<<"Hello World";
+    return 0;
+}
+
